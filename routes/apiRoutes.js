7d4e5cda2c38d66b8025a01daac9b90ecbba62c7
pphanouvong <pphanouvong1@gmail.com>
2018-08-30 20:55:36 -0400

    changed callbacks for htmlRoutes from examples to authors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function (app) {
   // Get all authors
   app.get("/api/authors", function (req, res) {
-    db.Author.findAll({}).then(function (dbAuthors) {
+    db.Authors.findAll({}).then(function (dbAuthors) {
       res.json(dbAuthors);
     });
   });
