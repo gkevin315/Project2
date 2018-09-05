521834ac03ea853f796364aeee8bb1604dfdd56f
Splumber <sajjadplumber@Sajjads-MacBook-Pro.local>
2018-09-04 21:34:23 -0400

    ready to merge with kevins passport

--- public/js/index.js
+++ public/js/index.js
@@ -25,6 +25,16 @@ var API = {
       data: JSON.stringify(post)
     });
   },
+  // saveCatergory: function(catergory){
+  //   return $.ajax({
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     type: "POST",
+  //     url: "api/category",
+  //     data: JSON.stringify(category)
+  //   });
+  // },
   getExamples: function() {
     return $.ajax({
       url: "api/posts",
@@ -38,6 +48,13 @@ var API = {
     });
   }
 };
+
+
+var getCategories = function(){
+  $("#categoriesdrop").append(post.category);
+}
+
+
 
       console.log("posts working");
 
@@ -66,7 +83,12 @@ var refreshExamples = function() {
         .addClass("btn btn-danger float-right delete")
         .text("ｘ");
 
+      var thumbsUp = $("<button>").addClass("material-icons thumbsup").text("thumb_up").attr("id", "thumbsup"); 
+
+      var thumbsDown = $("<button>").addClass("material-icons thumbsdown").text("thumb_down");  
       $li.append($button);
+      $li.append(thumbsUp);
+      $li.append(thumbsDown);
 
       return $li;
     });
@@ -114,6 +136,8 @@ var handleDeleteBtnClick = function() {
 };
 
 refreshExamples();
+// saveCatergory();
+getCategories();
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit, console.log("submit btn working"));
